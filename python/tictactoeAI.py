653bca894d9d00e76486133509f54f7087728b78
Jakob Rud Bernhardt <jakobrb@outlook.dk>
2024-11-26 17:26:37 +0100

    Update tictactoeAI.py

--- python/tictactoeAI.py
+++ python/tictactoeAI.py
@@ -1,6 +1,12 @@
 #################################################################
 ## "Tic Tac Toe AI" - finds the next optimal move for a player ##
 #################################################################
+"""
+The algorithm assumes that after it's move, the opponent will make the smartest possible move.
+Therefore, 
+"""
+# the algorithm assumes that the opponent makes the smartes possible moves, 
+
 # based on:
 # https://www.geeksforgeeks.org/finding-optimal-move-in-tic-tac-toe-using-minimax-algorithm-in-game-theory/
 
@@ -161,39 +167,76 @@ class tttAI():
 						bestMove = (i, j) 
 						bestVal = moveVal 
 
-		"""
-		print("The value of the best Move is :", bestVal) 
-		print() 
-		"""
 		
+		#print("The value of the best Move is :", bestVal) 
+		#print() 
+		"""
+		If bestVal == +10, the computer has a winning move.
+		If bestVal == -10, the opponent has a winning move.
+		If no moves are left and evaluate is 0, the game is a draw.
+		"""
+		if bestVal == 10:
+			print("Robot has a winning move!")
+		elif bestVal == -10:
+			print("Opponent has a winning move!")
 		return bestMove 
 	
 	def translateMove(self, move):
 		"""
-    	translate next move from (row,col) to (x,y,z) coordinates
+    	translate next move from (row,col) to (x,y,z) coordinates.
+		rows on negative y axis, col on x axis
 
     	:param move: (row,col) of next move
-    	:param cellWidth: width/height of board cells
-    	:param topLeft: coordinate of top-left board corner
     	:return: returns 3d coordinates [x,y,z]
     	""" 
 		
-		x = self.topleft[0] + self.cellWidth*(move[0]) + 0.5*self.cellWidth
-		y = self.topleft[1] - self.cellWidth*(move[1]) - 0.5*self.cellWidth
+		x = self.topleft[0] + self.cellWidth*(move[1]) + 0.5*self.cellWidth
+		y = self.topleft[1] - self.cellWidth*(move[0]) - 0.5*self.cellWidth
 		z = self.topleft[2]
 		point3d = [x, y, z]
 
 		return point3d
-
 	
+	def checkGameState(self):
+		"""
+    	This function checks the current state of the game.
+    	Returns "Robot wins", "Opponent wins", "Draw", or "Game continues"
+    	""" 
+
+		score = self.evaluate(self.board)
+
+		# Check if either player has won
+		if score == 10:
+			return "Robot wins"
+		elif score == -10:
+			return "Opponent wins"
+
+		# Check for a draw (no moves left)
+		if not any(None in row for row in self.board):  # No empty cells (None)
+			return "Draw"
+
+		return "Game continues"
 
 
 if __name__ == "__main__":
-	# create object
-	tictacrobot = tttAI()
-	# initally, the board is empty everywhere
+	# create tictactoe object:
+	tictacrobot = tttAI() # board always starts as empty
 
-	# we call the find best move, based on the current board
+	# the board is now updated using our advanced computer vision:
+	print("**Simulated update of the board**")
+	tictacrobot.board = [ 
+		[ True, False, True ], 
+		[ False, False, True ], 
+		[ None, None, None ] 
+	] 
+
+	# check state of game before you find best move!
+	# If result is not "Game continues", the game is over and should not proceed.
+	result = tictacrobot.checkGameState()
+	if result != "Game continues":
+		print(result)
+
+	# we call "find best move", based on the current board
 	bestMove = tictacrobot.findBestMove() 
 	print("The Optimal Move is :") 
 	print("ROW:", bestMove[0], " COL:", bestMove[1]) # (0,0) is top-left cell of grid
@@ -201,27 +244,17 @@ if __name__ == "__main__":
 	nextpoint = tictacrobot.translateMove(bestMove)
 	print("The next point is: ", nextpoint)
 
-	# the board is now updated using our advanced computer vision:
-	print()
+	# after "nextpoint" has been calculated, we send it to the inverse kinematic function...
+
+	# we use the suggested move:
 	print("**Simulated update of the board**")
-	print()
 	tictacrobot.board = [ 
 		[ True, False, True ], 
 		[ False, False, True ], 
-		[ None, None, None ] 
+		[ None, None, True ] 
 	] 
-
-	# The next move is calculated:
-	bestMove = tictacrobot.findBestMove() 
-	print("The Optimal Move is :") 
-	print("ROW:", bestMove[0], " COL:", bestMove[1])
-	nextpoint = tictacrobot.translateMove(bestMove)
-	print("The next point is: ", nextpoint)
-
-	# after "nextpoint" has been calculated, we send it to the inverse kinematic function...
-
-	# we could use the "bestval" variable to check if game is won by someone or is a draw (see documentation)
-	# but for now it's not used.
+	# and check the state:
+	print(tictacrobot.checkGameState())
 
 
 
